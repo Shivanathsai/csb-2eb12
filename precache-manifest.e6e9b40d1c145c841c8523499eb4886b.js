ada52e3ca70b055bac316c55bf1ceb3ebd256b5a
Code Sandbox <hello@codesandbox.io>
2021-08-01 18:52:22 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.e6e9b40d1c145c841c8523499eb4886b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e35ce1bfb4ef270f923a",
+    "url": "https://shivanathsai.github.io/csb-2eb12/static/js/main.e35ce1bf.chunk.js"
+  },
+  {
+    "revision": "389aa3e42612e74fb386",
+    "url": "https://shivanathsai.github.io/csb-2eb12/static/css/1.39e7f6b4.chunk.css"
+  },
+  {
+    "revision": "389aa3e42612e74fb386",
+    "url": "https://shivanathsai.github.io/csb-2eb12/static/js/1.389aa3e4.chunk.js"
+  },
+  {
+    "revision": "141c3de82adddd18a9ad",
+    "url": "https://shivanathsai.github.io/csb-2eb12/static/js/runtime~main.141c3de8.js"
+  },
+  {
+    "revision": "74e080c1aab43db03eaadfce57b583a1",
+    "url": "https://shivanathsai.github.io/csb-2eb12/index.html"
+  }
+];
